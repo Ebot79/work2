feeecbf8bc9473dce92d712fbe833bbe067948aa
Ebot79 <eash79@gmail.com>
2013-07-09 23:39:08 -0400

    corrected Location error on jobs/new

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,5 @@
 class Job < ActiveRecord::Base
-  attr_accessible :bill_date, :description_of_work, :job_name, :job_type, :labor, :materials, :paid_date, :priority, :work_order_dates
+  attr_accessible :bill_date, :description_of_work, :job_name, :job_type, :labor, :materials, :paid_date, :priority, :work_order_dates, :location_id
   
   belongs_to :location
   belongs_to :contact
